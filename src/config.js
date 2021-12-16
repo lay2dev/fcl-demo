41866c93f355d2fc14679be365ec0c25b61cfc1f
ItVen <751733381@qq.com>
2021-12-16 18:23:25 +0800

    access flow-wallet

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
-import * as fcl from "@blocto/fcl"
+import * as fcl from "@blocto/fcl";
 
-fcl.config()
+fcl
+  .config()
   // .put("accessNode.api", "http://localhost:8080") // local Flow emulator
   // .put("challenge.handshake", "http://localhost:8701/flow/authenticate") // local dev wallet
-  .put("challenge.scope", "email") // request for Email
   .put("accessNode.api", "https://access-testnet.onflow.org") // Flow testnet
-  .put("discovery.wallet", "https://flow-wallet-testnet.blocto.app/api/flow/authn") // Blocto testnet wallet
-  .put("discovery.wallet.method", "HTTP/POST")
-  .put("service.OpenID.scopes", "email!")
+  .put("discovery.wallet", "https://test.flow.unipass.id/") // Blocto testnet wallet
+  .put("discovery.wallet.method", "POP/RPC")
+  .put("discovery.wallet.method.default", "POP/RPC");
